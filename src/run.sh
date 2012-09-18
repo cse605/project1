cb8f2b703b1d42535e2a4df473e54d6686dcb1ff
Micheal <micheal@visionmasterdesigns.com>
2012-09-18 17:08:27 -0400

    modified run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,2 @@
-javac edu/buffalo/cse605/list/iface/*.java  edu/buffalo/cse605/list/*.java  
-      edu/buffalo/cse605/list/coarse/*.java    edu/buffalo/cse605/list/fine/*.java
-      edu/buffalo/cse605/main/*.java  edu/buffalo/cse605/test/*.java
+javac edu/buffalo/cse605/list/iface/*.java edu/buffalo/cse605/list/*.java edu/buffalo/cse605/list/coarse/*.java edu/buffalo/cse605/list/fine/*.java  edu/buffalo/cse605/test/*.java edu/buffalo/cse605/main/*.java
 java edu/buffalo/cse605/main/Main
