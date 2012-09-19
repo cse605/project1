e503f52a17cba55a7ad6591d9b06d3d1c5f10b33
Micheal <micheal@visionmasterdesigns.com>
2012-09-19 18:46:56 -0400

    adding read prev and read next benchmarks

---
+++ src/edu/buffalo/cse605/test/readNext.java
@@ -0,0 +1,19 @@
+package edu.buffalo.cse605.test;
+
+import edu.buffalo.cse605.list.fine.CursorFine;
+import edu.buffalo.cse605.list.fine.FDListFine;
+
+public class readNext  extends Thread{
+     public CursorFine<String> c;
+     int i;
+     public readNext(FDListFine<String> f, int i){
+    	 this.i = i;
+    	 c = f.reader(f.head());
+     }
+     
+     public void run(){
+    	 while(c.curr() != null) {
+    		 c.next();
+    	 }
+     }
+}
